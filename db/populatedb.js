9e0a500ffb957d2062335f4dbc37197c8bf933a1
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2025-06-24 20:58:35 -0500

    update populatedb.js with real data

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,34 +1,89 @@
+require("dotenv").config();
 const { PrismaClient } = require("../generated/prisma");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 async function main() {
   const image = await prisma.image.create({
     data: {
-      src: "https://w.wallhaven.cc/full/je/wallhaven-je82zp.jpg",
-      width: 3840,
-      height: 1920,
+      src: "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750737349/evbkszblprjybz0bvhqg.jpg",
+      width: 3631,
+      height: 2336,
     },
   });
 
   const characters = await prisma.character.createManyAndReturn({
     data: [
       {
-        name: "Bobby",
+        name: "2B",
         avatar:
-          "https://static.wikia.nocookie.net/wkbs-pbs-kids/images/c/c7/Bobby.png/revision/latest?cb=20210217232722",
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815845/uglqzyfkbavlg5pf7wkv.png",
         imageId: image.id,
       },
       {
-        name: "Master Chief",
+        name: "Clank",
         avatar:
-          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2331afef-593b-4a8e-900c-ce9e0be967fa/d1b8jn4-d069c0d0-0c9e-4122-9e52-ee29a8b24a41.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIzMzFhZmVmLTU5M2ItNGE4ZS05MDBjLWNlOWUwYmU5NjdmYVwvZDFiOGpuNC1kMDY5YzBkMC0wYzllLTQxMjItOWU1Mi1lZTI5YThiMjRhNDEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.hITWcXycN3D3n4DPBay0BBHCh-PtROcLQEjPPGDZcj0",
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815851/gpapvmu6kvi8n4ioc3nw.png",
+        imageId: image.id,
+      },
+      {
+        name: "Doomguy",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815859/ygheetrrosrcq1cqbze6.png",
+        imageId: image.id,
+      },
+      {
+        name: "Ico",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815865/ky6bmeyxmakmwlevma2t.png",
         imageId: image.id,
       },
       {
         name: "Isaac",
         avatar:
-          "https://static.wikia.nocookie.net/bindingofisaac/images/a/a0/IsaacHD.png/revision/latest/thumbnail/width/360/height/450?cb=20120617101324",
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815870/omuqx7bofbg6xbl9sjvk.png",
+        imageId: image.id,
+      },
+      {
+        name: "Jim Raynor",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815877/yidjkttq9uwcbfg1lm6v.png",
+        imageId: image.id,
+      },
+      {
+        name: "Kratos",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815882/zbcmuoyb0tqvko7uyxyc.png",
+        imageId: image.id,
+      },
+      {
+        name: "Manny Calavera",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815887/p20pqfhhafnrqnjyvqdh.png",
+        imageId: image.id,
+      },
+      {
+        name: "Marcus Fenix",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815891/h65ttq0jt6bvbq0ypxe4.png",
+        imageId: image.id,
+      },
+      {
+        name: "The Prince",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815895/aazdddrii24nytipmd95.png",
+        imageId: image.id,
+      },
+      {
+        name: "Solaire of Astora",
+        avatar:
+          "https://res.cloudinary.com/dwf29bnr3/image/upload/v1750815900/i5mlfs3uv6vbrbz7v9ig.png",
         imageId: image.id,
       },
     ],
@@ -36,19 +91,17 @@ async function main() {
 
   await prisma.location.createMany({
     data: [
-      { x: 10, y: 20, characterId: characters[0].id },
-      { x: 200, y: 400, characterId: characters[1].id },
-      { x: 400, y: 60, characterId: characters[2].id },
-    ],
-  });
-
-  const game = await prisma.game.create({ data: { imageId: image.id } });
-
-  await prisma.objective.createMany({
-    data: [
-      { characterId: characters[0].id, gameId: game.id },
-      { characterId: characters[1].id, gameId: game.id },
-      { characterId: characters[2].id, gameId: game.id },
+      { x: 949, y: 1342, characterId: characters[0].id },
+      { x: 2842, y: 1951, characterId: characters[1].id },
+      { x: 1741, y: 766, characterId: characters[2].id },
+      { x: 1275, y: 1679, characterId: characters[3].id },
+      { x: 1948, y: 954, characterId: characters[4].id },
+      { x: 2901, y: 705, characterId: characters[5].id },
+      { x: 2767, y: 1623, characterId: characters[6].id },
+      { x: 674, y: 1535, characterId: characters[7].id },
+      { x: 426, y: 1276, characterId: characters[8].id },
+      { x: 1906, y: 2204, characterId: characters[9].id },
+      { x: 1558, y: 1320, characterId: characters[10].id },
     ],
   });
 }
